1b25b3efdf1c043aa4577ad826dccb787c211b6a
Loïc <loic@jeedom.com>
2016-08-09 16:50:25 +0200

    remove test

--- backup_dir.sh
+++ backup_dir.sh
@@ -9,37 +9,32 @@ ssh_src=$(echo ${rsync_src} | grep -c "@")
 ssh_cib=$(echo ${rsync_cib} | grep -c "@")
 
 if [ ${ssh_src} -eq  0 ]; then
-        if   ! [ -d ${rsync_src} ] && ! [ -f ${rsync_src} ]; then
-                echo "Repertoire source ou fichier introuvable"
-                exit 1
-        fi
+    if   ! [ -d ${rsync_src} ] && ! [ -f ${rsync_src} ]; then
+            echo "Repertoire source ou fichier introuvable"
+            exit 1
+    fi
 
-        if [ `find ${rsync_src} -type f | wc -l` -eq 0 ] ; then
-                echo "Repertoire source vide"
-                exit 1
-        fi
+    if [ `find ${rsync_src} -type f | wc -l` -eq 0 ] ; then
+            echo "Repertoire source vide"
+            exit 1
+    fi
 fi
 
 if [ ${ssh_cib} -eq  0 ]; then
-        if ! [ -d ${rsync_cib} ]; then
-                echo "Repertoire cible introuvable"
-                exit 1
-        fi
+    if ! [ -d ${rsync_cib} ]; then
+            echo "Repertoire cible introuvable"
+            exit 1
+    fi
 
-        if ! [ -r ${rsync_cib} ]; then
-                echo "Repertoire illisible"
-                exit 1
-        fi
+    if ! [ -r ${rsync_cib} ]; then
+            echo "Repertoire illisible"
+            exit 1
+    fi
 
-        if ! [ -w ${rsync_cib} ]; then
-                echo "Repertoire non modifiable"
-                exit 1
-        fi
-
-        if [ `find ${rsync_cib} -type f | wc -l` -eq 0 ] ; then
-                echo "Repertoire cible vide veuillez lancer la premiere synchronisation manuellement"
-                exit 1
-        fi
+    if ! [ -w ${rsync_cib} ]; then
+            echo "Repertoire non modifiable"
+            exit 1
+    fi
 fi
 
 rsync -rltgoDphv --force --delete-after --no-perms --no-owner --no-group  ${rsync_src} ${rsync_cib} >> /dev/null
