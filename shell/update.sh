3b9894770b498bb1c44c1b795d92410457c5e3fb
Loïc <loic@jeedom.com>
2016-12-21 15:19:33 +0100

    Update update.sh

--- shell/update.sh
+++ shell/update.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
+PWD=$(pwd)
+cd ${DIR}
 git reset --hard HEAD
 git pull
 find ${DIR} -iname "*.sh" -type f -exec dos2unix {} \;
 find ${DIR} -iname "*.sh" -type f -exec chmod +x {} \;
+cd ${PWD}
