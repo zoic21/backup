1628a105b65377fe9e7be84db19f05908ecdee2a
Loïc <loic@jeedom.com>
2017-01-04 11:32:17 +0100

    Add files via upload

---
+++ shell/backup/backup_github.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+LOGIN=TODO
+TOKEN=TODO
+ORG=TODO
+DONE=0
+PAGE=0
+DIR_BCK="/todo"
+
+NUMREPOS=$(curl -s "https://$LOGIN:$TOKEN@api.github.com/orgs/$ORG" | awk '
+   /public_repos/{t+=$2}
+   /total_private_repos/{t+=$2}
+   END{print t}')
+
+echo "Num repos : ${NUMREPOS}"
+
+while [ ${DONE} -lt ${NUMREPOS} ]; do
+   for REPO in $(curl -s "https://$LOGIN:$TOKEN@api.github.com/orgs/$ORG/repos?type=all&page=$PAGE" | awk '/full_name/{print substr($2, 2, length($2)-3)}'); do
+        echo "https://api.github.com/repos/${REPO}/zipball/"
+        curl -s -H "Authorization: token ${TOKEN}" -L "https://api.github.com/repos/${REPO}/zipball/" > ${DIR_BCK}/${REPO}.zip
+        if [ $? -ne 0 ]; then
+                echo "Error on ${REPO}"
+        fi
+        DONE=$(($DONE + 1))
+        echo "Repo ${DONE}/${NUMREPOS}"
+   done
+   PAGE=$(($PAGE + 1))
+   echo "Page ${PAGE}"
+done
